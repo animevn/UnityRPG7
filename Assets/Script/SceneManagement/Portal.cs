ba780575421d01e0c836144b475d5ba3bdbcd788
animevn <haanhth21@gmail.com>
2020-04-15 20:38:10 +0700

    Load scene ok now

---
+++ Assets/Script/SceneManagement/Portal.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Script.SceneManagement
+{
+    public class Portal : MonoBehaviour
+    {
+        [SerializeField] private int sceneToLoad = -1;
+
+        private static bool IsPlayerCollider(Component player)
+        {
+            return player.CompareTag("Player");
+        }
+        
+        private void OnTriggerEnter(Collider other)
+        {
+            if (IsPlayerCollider(other))
+            {
+                SceneManager.LoadScene(sceneToLoad, LoadSceneMode.Single);
+            }
+        }
+    }
+}
