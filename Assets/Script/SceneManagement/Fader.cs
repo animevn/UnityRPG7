ae44270bd3b1a0375604c34f5aa9df0d36b59240
animevn <haanhth21@gmail.com>
2020-04-16 09:46:18 +0700

    fade screen ok

---
+++ Assets/Script/SceneManagement/Fader.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+
+namespace Script.SceneManagement
+{
+    public class Fader : MonoBehaviour
+    {
+        [SerializeField] private float timeFade;
+
+        private CanvasGroup canvas;
+        
+        private void Start()
+        {
+            canvas = GetComponent<CanvasGroup>();
+            StartCoroutine(FadeOut(timeFade));
+        }
+
+        private IEnumerator FadeOut(float time)
+        {
+            while (Math.Abs(canvas.alpha - 1) > 0)
+            {
+                canvas.alpha += Time.deltaTime / time;
+                yield return null;
+            }
+        }
+    }   
+}
