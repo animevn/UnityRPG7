3b418f5f85dc33e41ba7b36e6c567fac77bfd5ae
animevn <haanhth21@gmail.com>
2020-04-16 09:51:07 +0700

    fade in out ok

--- Assets/Script/SceneManagement/Fader.cs
+++ Assets/Script/SceneManagement/Fader.cs
@@ -13,7 +13,15 @@ namespace Script.SceneManagement
         private void Start()
         {
             canvas = GetComponent<CanvasGroup>();
-            StartCoroutine(FadeOut(timeFade));
+            StartCoroutine(FadeOutIn(timeFade));
+        }
+
+        private IEnumerator FadeOutIn(float time)
+        {
+            yield return FadeOut(time);
+            print("Fadeout OK");
+            yield return FadeIn(time);
+            print("Fadein OK");
         }
 
         private IEnumerator FadeOut(float time)
@@ -21,6 +29,15 @@ namespace Script.SceneManagement
             while (Math.Abs(canvas.alpha - 1) > 0)
             {
                 canvas.alpha += Time.deltaTime / time;
+                yield return null;
+            }
+        }
+        
+        private IEnumerator FadeIn(float time)
+        {
+            while (Math.Abs(canvas.alpha) > 0)
+            {
+                canvas.alpha -= Time.deltaTime / time;
                 yield return null;
             }
         }
